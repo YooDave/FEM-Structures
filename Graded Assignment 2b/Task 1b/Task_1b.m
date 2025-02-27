612771c5b4d2bbfd0599271d18515464196ff121
Brijeshn925 <brijeshn925@outlook.com>
2025-02-27 18:52:37 +0100

    heyy 222

--- Graded Assignment 2b/Task 1b/Task_1b.m
+++ Graded Assignment 2b/Task 1b/Task_1b.m
@@ -43,7 +43,29 @@ elcoord=[Ex(:,1) Ey(:,1) ; Ex(:,2) Ey(:,2)  ;Ex(:,3) Ey(:,3) ; Ex(:,4) Ey(:,4) ]
 
 Xcoord = elcoord(:,1); Ycoord = elcoord(:,2);
 
-LeftSide_nodes=find(abs(Xcoord-L)<100*eps)';
+LeftSide_nodes=find(abs(Xcoord-L)'<100*eps);
+RightSide_nodes=find(abs(Xcoord-0)'<100*eps);
+BottomSide_nodes=find(abs(Ycoord-H)'<100*eps);
+TopSide_nodes=find(abs(Ycoord-0)'<100*eps); % this logic computed from TunnelMeshGen.m from CE1
+
+platform_nodes1 = find(Xcoord(TopSide_nodes)<(l + (L - l)/2))';
+platform_nodes = find(Xcoord(platform_nodes1)>(L - l)/2);
+
+No_of_nodes_in_topXcoord = length(Xcoord); % total number of nodes
+all_nodes = 1:No_of_nodes_in_topXcoord;
+
+excluded_top_nodes = setdiff(all_nodes, platform_nodes); % Using setdiff
+
+ndofs_ip = 2*length(Coord(:,1));
+ndofs_op = 3*length(Coord(:,1));
+
+dofs = 1:ndofs_ip;
+
+% dof_left=zeros(2*length(LeftSide_nodes),1);
+% for i = 1:length(LeftSide_nodes)
+%     dof_left(i:i+1) = [(LeftSide_nodes(i)-1)*2 + 1;(LeftSide_nodes(i)-1)*2];
+%     i=i+1;
+% end
 
 Dbar=D*t^3/12;
 
